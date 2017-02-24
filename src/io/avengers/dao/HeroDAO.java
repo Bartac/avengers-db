c8159a63b8a96984a7d88da7c297e3148999dbe4
jbog77 <bogota.jeremy@gmail.com>
2017-02-24 11:25:47 +0100

    Find by name

--- src/io/avengers/dao/HeroDAO.java
+++ src/io/avengers/dao/HeroDAO.java
@@ -12,30 +12,30 @@ import io.avengers.domain.Hero;
 import io.avengers.domain.Sex;
 
 public class HeroDAO {
+	static Class c;
 
-	public Set<Hero> findAll() throws ClassNotFoundException, SQLException {
+	public HeroDAO() {
+
+		if (c == null) {
+			try {
+				c = Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				throw new IllegalStateException("SQLDriver is not here: " + e.getMessage());
+			}
+		}
+
+	}
+
+	public Set<Hero> findAll() throws SQLException {
 
 		String query = "SELECT * FROM `heroes`";
-
-		Class.forName("com.mysql.jdbc.Driver");
-		// 3306 no password
-		Connection connect = null;
-		connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/marvel", "root", "");
-
+		Connection connect = connectToMySQL();
 		Statement statement = connect.createStatement();
 		ResultSet resultSet = statement.executeQuery(query);
 		Set<Hero> heroes = new HashSet<>();
 
 		while (resultSet.next()) {
-			int id = resultSet.getInt("id");
-			String name = resultSet.getString("name");
-			String sSex = resultSet.getString("sex");
-			long likes = resultSet.getLong("likes");
-			long dislikes = resultSet.getLong("dislikes");
-
-			Hero h = new Hero(id, name, Sex.O, likes, dislikes);
-			heroes.add(h);
-
+			heroes.add(resultSetToHero(resultSet));
 		}
 
 		connect.close();
@@ -43,4 +43,50 @@ public class HeroDAO {
 
 	}
 
+	public Set<Hero> findHeroesByName(String term) throws SQLException {
+
+		String query = "SELECT h.name, h.sex, i.name, m.name, t.team_name, h.picture, h.abilities, h.history, t.picture FROM heroes h INNER JOIN movie_hero mh ON h.id = mh.id_hero INNER JOIN movie m ON mh.id_movie = m.id INNER JOIN team_hero th ON th.hero_id = h.id INNER JOIN team t ON t.team_id = th.team_id INNER JOIN irl i ON i.hero_id = h.id WHERE h.name = '"
+				+ term + "";
+
+		Connection connect = connectToMySQL();
+		Statement statement = connect.createStatement();
+		ResultSet resultSet = statement.executeQuery(query);
+		Set<Hero> heroes = new HashSet<>();
+
+		while (resultSet.next()) {
+			heroes.add(resultSetToHero(resultSet));
+		}
+
+		connect.close();
+		return heroes;
+
+	}
+
+	Hero resultSetToHero(ResultSet resultSet) {
+
+		try {
+			int id = resultSet.getInt("id");
+			String name = resultSet.getString("name");
+			String sSex = resultSet.getString("sex");
+			long likes = resultSet.getLong("likes");
+			long dislikes = resultSet.getLong("dislikes");
+			Hero h = new Hero(id, name, Sex.O, likes, dislikes);
+			return h;
+		} catch (SQLException e) {
+			throw new IllegalStateException("Database Broken: " + e.getMessage());
+		}
+
+	}
+
+	public Connection connectToMySQL() {
+		// 3306 no password
+		Connection connect;
+		try {
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/marvel", "root", "");
+			return connect;
+		} catch (SQLException e) {
+			throw new IllegalStateException("Wrong credentials or url, or overloaded connection: " + e.getMessage());
+		}
+
+	}
 }
--- src/io/avengers/service/HeroService.java
+++ src/io/avengers/service/HeroService.java
@@ -1,15 +1,41 @@
 package io.avengers.service;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 import io.avengers.dao.HeroDAO;
 import io.avengers.domain.Hero;
 
 public class HeroService {
-	
-	public Set<Hero> findAll() throws Exception{
-		return new HeroDAO().findAll();
-		
+	IllegalStateException stateException = new IllegalStateException("Error, contact your admin");
+
+	public Set<Hero> findAll() {
+		try {
+			return new HeroDAO().findAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw stateException;
+		}
+	}
+
+	public Set<Hero> findHeroesByName(String term) {
+
+		// Guards
+		if (term == null) {
+			System.out.println("Potential Bug or illegal requesta");
+			return this.findAll();
+		}
+		if (term.isEmpty()) {
+			return this.findAll();
+		}
+
+		// Method
+		try {
+			return new HeroDAO().findHeroesByName(term);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw stateException;
+		}
 	}
 
 }
