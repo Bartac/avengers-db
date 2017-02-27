f00534e568ea7b65d9c0d241b77eacf9ad04a56e
Bartac <remy.mainard@orange.fr>
2017-02-27 15:38:41 +0100

    COMMIT UPDATE TESTS

--- src/io/avengers/domain/Hero.java
+++ src/io/avengers/domain/Hero.java
@@ -16,6 +16,15 @@ public class Hero {
 		this.abilities = abilities;
 		this.history = history;
 	}
+	
+	
+
+	public Hero(String name) {
+		super();
+		this.name = name;
+	}
+
+
 
 	@Override
 	public String toString() {
@@ -68,6 +77,31 @@ public class Hero {
 
 	public void setHistory(String history) {
 		this.history = history;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hero other = (Hero) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
---
+++ test/io/avengers/dao/HeroDaoTest.java
@@ -0,0 +1,55 @@
+package io.avengers.dao;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.avengers.domain.Hero;
+
+public class HeroDaoTest {
+
+	HeroDAO dao;
+	Connection connect;
+	
+	@Before
+	public void setUp() throws Exception {
+		dao = new HeroDAO();
+		connect = dao.connectToMySQL();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		connect.close();
+	}
+
+	@Test
+	public void testHeroDAO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() throws SQLException {
+		assertTrue(dao.findAll().size() > 5);
+	}
+
+	@Test
+	public void testFindHeroesByName() throws SQLException {
+		//Set<Hero> hero = dao.findHeroesByName("Hulk");
+		Hero hero = new Hero("Antman");
+		
+		assertTrue(dao.findHeroesByName("man").contains(hero));
+	}
+
+	@Test
+	public void testResultSetToHero() {
+		fail("Not yet implemented");
+	}
+
+}
