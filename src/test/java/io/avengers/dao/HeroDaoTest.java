090adbc81ab631a3fe05abb72760eed89a44af2d
Bartac <remy.mainard@orange.fr>
2017-03-30 11:41:34 +0200

    COMMIT TEST

--- src/test/java/io/avengers/dao/HeroDaoTest.java
+++ src/test/java/io/avengers/dao/HeroDaoTest.java
@@ -54,5 +54,66 @@ public class HeroDaoTest {
 		assertTrue(heroes.contains(new Hero("Captain America")));
 		assertTrue(heroes.contains(new Hero("loki")));
 	}
+	
+	@Test
+	public void testFindHeroesById() throws SQLException {
+		Hero heros = dao.findHeroesById("1");
+		
+		//Spiderman's id = 1 (BDD)
+		//check if it's true
+		assertTrue(heros.equals(dao.findHeroesByName("Spiderman").iterator().next()));
+		
+		//check if with an other hero it's false
+		assertFalse(heros.equals(dao.findHeroesByName("Hulk").iterator().next()));
+		
+	}
+	
+	@Test
+	public void testCreateHero() throws SQLException{
+		Integer id = dao.createHero("toto", "jack");
+		
+		assertTrue(id == dao.findHeroesById(id.toString()).getId());
+		
+		dao.deleteHero(id);
+		
+		assertTrue(dao.findHeroesById(id.toString()) == null);
+		
+	}
+	
+	@Test
+	public void testAddHeroToTeam() throws SQLException{
+		Integer id = dao.createHero("toto", "jack");
+		
+		dao.addHeroToTeam("Avengers", "toto");
+		
+		assertTrue(dao.findHeroesByName("toto").iterator().next().getTeam_name().equals("Avengers"));
+		assertFalse(dao.findHeroesByName("toto").iterator().next().getTeam_name().equals("Xmen"));
+		
+		dao.deleteHero(id);
+	}
+	
+	@Test
+	public void testAddHeroToMovie() throws SQLException{
+		Integer id = dao.createHero("toto", "jack");
+		
+		dao.addHeroToMovie(1, id);
+		
+		assertTrue(dao.findHeroesByName("toto").iterator().next().getMovies_name().iterator().next().equals("Iron Man"));
+		assertFalse(dao.findHeroesByName("toto").iterator().next().getMovies_name().iterator().next().equals("Captain"));
+		
+		dao.deleteHero(id);
+	}
+	
+	@Test
+	public void testUpdateHero() throws SQLException{
+		Integer id = dao.createHero("toto", "jack");
+		
+		dao.updateHero(id, "tintin");
+		
+		assertTrue(dao.findHeroesById(id.toString()).getName().equals("tintin"));
+		assertFalse(dao.findHeroesById(id.toString()).getName().equals("toto"));
+		
+		dao.deleteHero(id);
+	}
 
 }
